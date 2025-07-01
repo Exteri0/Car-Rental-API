8cedee8baddcf20403088854f4890273645ba650
Exteri0 <omarfaramawy19@gmail.com>
2025-07-01 12:16:10 +0300

    added routes (no functionality yet)

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpCode, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,29 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('car')
+  helloworld(): string {
+    return 'HELLO THIS IS CAR';
+  }
+
+  @Get('notfound')
+  @HttpCode(404)
+  notFound(): string {
+    return 'This is a 404 page';
+  }
+
+  @Get('cat/:id')
+  getCatById(@Param('id') id: string): string {
+    return 'Now we show cat with id: ' + id;
+  }
+}
+
+@Controller({ host: 'admin.example.com' })
+export class AdminController {
+  @Get('admin')
+  index(): string {
+    return 'Admin page';
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { AdminController, AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { TransactionsModule } from './transactions/transactions.module';
+import { CarsModule } from './cars/cars.module';
+import { RentalsModule } from './rentals/rentals.module';
+import { ReviewsModule } from './reviews/reviews.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [UsersModule, RentalsModule, CarsModule, TransactionsModule, ReviewsModule],
+  controllers: [AppController, AdminController],
   providers: [AppService],
 })
 export class AppModule {}
